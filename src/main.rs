a021fd435cea03b575d0f32fa39a12352d4f2846
Nathan Li <nathan.li.1995@gmail.com>
2024-04-21 19:32:26 -0400

    refactored main to use modules and read configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    // panic if can't read config file
+    let configuration = get_configuration().expect("Failed to read config file");
+    // init app
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
