140e5621e11048aa56ec2e938c346f142393e4d1
Nathan Li <nathan.li.1995@gmail.com>
2025-09-14 15:04:17 -0400

    fix(github actions/general): skip user creation from init_db if github action

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -55,28 +55,29 @@ if [ "${SKIP_DOCKER}" = "false" ]; then
     --detach \
     --name "${CONTAINER_NAME}" \
     postgres -N 1000 # increase max num of connections to 1000 for testing purposes
+
+  # wait until db is ready before continue
+  MAX_WAIT=60
+  WAITED=0
+  until [ "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == "healthy" ]; do
+    if [ "$WAITED" -ge "$MAX_WAIT" ]; then
+      >&2 echo "Postgres did not become healthy after ${MAX_WAIT} seconds. Failing."
+      exit 1
+    fi
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 2
+    WAITED=$((WAITED + 2))
+  done
+  >&2 echo "Postgres container is ready on port ${DB_PORT}"
+
+  CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPER_USER}" -c "${CREATE_QUERY}"
+
+  # create newsletter table and grant privileges
+  GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPER_USER}" -c "${GRANT_QUERY}"
 fi
 
-# wait until db is ready before continue
-MAX_WAIT=60
-WAITED=0
-until [ "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == "healthy" ]; do
-  if [ "$WAITED" -ge "$MAX_WAIT" ]; then
-    >&2 echo "Postgres did not become healthy after ${MAX_WAIT} seconds. Failing."
-    exit 1
-  fi
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 2
-  WAITED=$((WAITED + 2))
-done
->&2 echo "Postgres container is ready on port ${DB_PORT}"
-
-CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPER_USER}" -c "${CREATE_QUERY}"
-
-# create newsletter table and grant privileges
-GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPER_USER}" -c "${GRANT_QUERY}"
 
 DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@${DB_HOST}:${DB_PORT}/${APP_DB_NAME}
 DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
